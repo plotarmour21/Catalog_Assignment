cd2d89467289cb16d84eecb240f6ccbf1d6e0cff
plotarmour21 <sumith2107@gmail.com>
2024-10-17 18:45:22 +0530

    Adding project files assignment implementation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "./Component/Menu";
+import Price from "./Component/Price";
+
+export default function App() {
+  return (
+    <div className="">
+      <Price />
+      <Menu />
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    circular: ["CircularStd", "sans-serif"],
+    manrope: ["Manrope", "sans"],
+    sans: ['Inter', 'sans-serif'],
+  },
+  plugins: [],
+};
